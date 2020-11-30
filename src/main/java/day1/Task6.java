ccefcc8551b0484c001eb1bcf25a731636f6430c
skityba <kk@mm.st>
2020-11-30 09:26:17 +0300

    Add tasks for Day1 solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+        for (int m = 1; m <= 9; m++) {
+            System.out.println(m + " * " + k + " = " + (k * m));
+        }
     }
 }
