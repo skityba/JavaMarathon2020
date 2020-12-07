ded03122aba82a70f9989364bfc5cdd54f82fe2c
skityba <kk@mm.st>
2020-12-07 18:35:02 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setYear(1968);
+        car.setColor("white");
+        car.setModel("Citroen");
+        System.out.println("Год производства: " + car.getYear());
+        System.out.println("Цвет: " + car.getColor());
+        System.out.println("Модель: " + car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,9 @@
 package day5;
 
-
 public class Task2 {
     public static void main(String[] args) {
-
+     Motorbike bike = new Motorbike(1980, "black", "Yamaha");
+        System.out.println("Мой мотоцикл марки " + bike.getModel() + ", он цвета " + bike.getColor() + " и собран в " + bike.getYear());
     }
+
 }
