71fbd8c1df41b061e67a8d3179743736aeb19579
skityba <kk@mm.st>
2020-12-09 16:49:21 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,28 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.nanoTime();
+        String s = "";
+        for (int i = 0; i < 20001; i++) {
+            s = s + i  + " ";
+        }
+        System.out.print(s);
+        long finish = System.nanoTime();
 
+        long start2 = System.nanoTime();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 20001; i++) {
+            sb.append(i + " ");
+        }
+        System.out.print(sb);
+        long finish2 = System.nanoTime();
+        System.out.println();
+        System.out.println(start);
+        System.out.println(finish);
+        System.out.println("Время выполнения конкатенации строк: " + (finish - start));
+        System.out.println();
+        System.out.println(start2);
+        System.out.println(finish2);
+        System.out.println("Время выполнения обьединения строк при помощи StringBuilder: " + (finish2 - start2));
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplaine aircraft = new Airplaine("Fokker", 2000, 150, 20000, 0);
+        System.out.println(aircraft);
 
     }
 }
