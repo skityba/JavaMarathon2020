917672a0b11bf0c66d80b3d375551c3e2ce0c569
skityba <kk@mm.st>
2020-12-17 00:45:11 +0300

    complete day13 tasks

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -1,7 +1,27 @@
 package day13;
 
+import static day13.MessageDatabase.showDialog;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        User user1 = new User("Cole");
+        User user2 = new User("Peter");
+        User user3 = new User("Alex");
+
+        user1.sendMessage(user2, "hello!");
+        user1.sendMessage(user2, "hi!");
+        user2.sendMessage(user1, "hello!");
+        user2.sendMessage(user1, "hi!");
+        user2.sendMessage(user1, "hurra!");
+        user3.sendMessage(user1, "hello!");
+        user3.sendMessage(user1, "hi!");
+        user3.sendMessage(user1, "hurra!");
+        user1.sendMessage(user3, "hello!");
+        user1.sendMessage(user3, "hi!");
+        user1.sendMessage(user3, "hurra!");
+        user3.sendMessage(user1, "salut!");
+
+        showDialog(user1, user3);
     }
 }
---
+++ src/main/java/day13/User.java
@@ -0,0 +1,43 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+
+    public User(String username) {
+        this.username = username;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void subscribe(User user) {
+        subscriptions.add(new User(user.getUsername()));
+    }
+
+    public boolean isSubscribed(User user) {
+        return this.subscriptions.contains(user);
+    }
+
+    public boolean isFriend(User user) {
+        return this.isSubscribed(user) && user.isSubscribed(User.this);
+    }
+
+    public void sendMessage(User user, String text) {
+        MessageDatabase.addNewMessage(User.this, user, text);
+    }
+
+    public String toString() {
+        return username;
+    }
+}
+
