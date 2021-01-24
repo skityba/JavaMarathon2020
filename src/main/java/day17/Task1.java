fce72b80342dc1ab0d5c3a6c4e486d6f97d8e514
skityba <kk@mm.st>
2021-01-24 20:11:32 +0300

    day17 1 task

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,13 @@
 package day17;
 
+import static day17.ChessPiece.*;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] figures = {PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, ROOK_BLACK, ROOK_BLACK,
+                ROOK_BLACK, ROOK_BLACK};
+        for(int i = 0; i < 8; i++) {
+            System.out.print(figures[i] + " ");
+        }
     }
 }
