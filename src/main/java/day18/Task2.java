54e6c31adb3efb34ca7144a11bd7626df1e46508
skityba <kk@mm.st>
2021-02-03 19:12:30 +0300

    correct day18 task2

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -9,17 +9,16 @@ public class Task2 {
 
     public static int count7(int number){
 
-//        int countNumber = number % 10;      мое решение со счетчиком
-//        int count = 0;
-//        if (countNumber == 7) count = 1;
-//        if(number / 10 < 1 && number != 7) return 0;
-//        return count + count7(number / 10);
+        int count = 0;
+        if(number % 10 == 7) count = 1;    //мое решение со счетчиком, но без циклов
+        if(number / 10 < 1 && number != 7) return 0;
+        return count + count7(number / 10);
 
-            if (number == 0) return 0;   // правильное решение без счетчика
-            if (number % 10 == 7)
-                return 1 + count7(number / 10);
-            else
-                return count7(number / 10);
+//            if (number == 0) return 0;   // правильное(?) решение без счетчика
+//            if (number % 10 == 7)
+//                return 1 + count7(number / 10);
+//            else
+//                return count7(number / 10);
         }
     }
 
